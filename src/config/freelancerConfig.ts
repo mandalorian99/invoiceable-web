ce45f62299b142b64d81589554c6695b67a50d7e
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2025-03-30 00:32:20 +0530

    hot fix

--- src/config/freelancerConfig.ts
+++ src/config/freelancerConfig.ts
@@ -1,4 +1,10 @@
-
+/**
+ * @license
+ * Invoiceable - Open Source Invoice Builder
+ * Copyright (C) 2024 Mahendra Choudhary
+ * AGPL-3.0 License with commercial terms
+ * https://github.com/mandalorian99/invoiceable-web
+ */
 import { InvoiceTemplateConfig } from '../types/invoice';
 
 export const freelancerConfig: InvoiceTemplateConfig = {
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+/**
+ * @license
+ * Invoiceable - Open Source Invoice Builder
+ * Copyright (C) 2024 Mahendra Choudhary
+ * AGPL-3.0 License with commercial terms
+ * https://github.com/mandalorian99/invoiceable-web
+ */
 /// <reference types="vite/client" />
